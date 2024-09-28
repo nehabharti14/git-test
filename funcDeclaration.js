f2cf15cdfe055dc7ce88fbf9dea6de9e4ce5c4f2
nehabharti14 <nehabharti14205@gmail.com>
2024-09-28 12:37:12 +0530

    function_declaration_

--- funcDeclaration.js
+++ funcDeclaration.js
@@ -4,3 +4,13 @@ function temperature(f){
 }
 temp1=temperature(77);
 console.log("this is today's temperature = "+temp1);
+
+
+
+console.log("*********************OR IN OTHERWAY****************");
+function today(f){
+    tmprtr=(5/6)*(f-32);
+    return tmprtr;
+}
+let result= "today's temperature is ="+ today(85,69,76)+  " Centigrate";
+console.log(result);
